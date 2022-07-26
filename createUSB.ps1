c420769bb5c91741ce8eb012b591bac7bb0c03c5
Jacek Bylina <jbylina@users.noreply.github.com>
2022-07-26 18:41:48 +0200

    fixes

--- createUSB.ps1
+++ createUSB.ps1
@@ -1,7 +1,3 @@
 
 
-
-
-Edit-OSDCloud.winpe -WebPSScript https://raw.githubusercontent.com/ukrainski-dom/OSDCloud-config/main/base.ps1 -PSModuleInstall OSD
-
-
+Edit-OSDCloudWinPE -WebPSScript https://raw.githubusercontent.com/ukrainski-dom/OSDCloud-config/main/base.ps1 -PSModuleInstall OSD,AzureAD,WindowsAutoPilotIntune,Microsoft.Graph.Intune
