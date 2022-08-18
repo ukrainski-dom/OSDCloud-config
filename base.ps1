b3915bd80e66488f1db1cb92aaa3f0a63c72ec79
Jacek Bylina <jbylina@users.noreply.github.com>
2022-08-18 12:45:52 +0200

    Add oobe fix script for W10

--- base.ps1
+++ base.ps1
@@ -75,6 +75,106 @@ $OOBEDeployJson = @'
 $OOBEDeployJson | Out-File -FilePath 'C:\ProgramData\OSDeploy\OSDeploy.OOBEDeploy.json' -Encoding ascii -Width 2000 -Force
 
 
+$OOBEFixScript = @'
+##########################
+#trying to get the file #
+########################
+
+$Account = New-Object -TypeName System.Security.Principal.NTAccount -ArgumentList 'builtin\Administrators';
+
+try
+{
+	$Itemexists = test-path 'C:\Windows\SystemApps\*\webapps\inclusiveOobe\view\oobelocalaccount-main.html'
+	$ItemList = Get-Item -Path C:\Windows\SystemApps\*\webapps\inclusiveOobe\view\oobelocalaccount-main.html
+}
+catch
+{
+	write-host "an error occurred"
+        exit 1
+}
+
+
+
+####################################
+# change owner of the file         #
+#####################################
+
+
+if($Itemexists)
+{ 
+
+	$Acl = $null; 
+    	$Acl = Get-Acl -Path $Itemlist.FullName; 
+    	$Acl.SetOwner($Account); 
+    	Set-Acl -Path $Itemlist.FullName -AclObject $Acl; 
+}else{ 
+	Write-Host  "File not found!"
+        exit 1            
+}
+
+
+###########################################
+#Change acl permissions                  #
+###########################################
+
+
+try
+{
+	$Acl = Get-Acl -Path $Itemlist.FullName; 
+        $owner = $acl.owner
+}
+catch
+{
+	write-host "owner not found"
+        exit 1
+}
+
+
+
+if ($owner -eq $account)      
+{
+   	$myPath = $itemlist
+	$myAcl = Get-Acl "$myPath"
+	$myAclEntry = "nt authority\system","FullControl","Allow"
+	$myAccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule($myAclEntry)
+	$myAcl.SetAccessRule($myAccessRule)
+	$myAcl | Set-Acl "$MyPath"
+	
+
+	$myPath = $itemlist
+	$myAcl = Get-Acl "$myPath"
+	$myAclEntry = $account,"FullControl","Allow"
+	$myAccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule($myAclEntry)
+	$myAcl.SetAccessRule($myAccessRule)
+	$myAcl | Set-Acl "$MyPath"
+	
+}else{
+       Write-Host "Permissions couldnt be changed"
+       exit 1
+}
+
+
+
+###############################################
+#remove the option to add a local account in oobe  #
+##############################################
+
+$data = foreach($line in Get-Content $itemlist)
+{
+    if($line -like '*/webapps/inclusiveOobe/js/oobelocalaccount-page.js*')
+    {
+    }
+    else
+    {
+        $line
+    }
+}
+$data | Set-Content $itemlist -Force
+exit 0
+'@
+$OOBEFixScript | Out-File -FilePath 'C:\ProgramData\OSDeploy\OOBEFixScript.ps1' -Encoding ascii -Width 2000 -Force
+
+
 #================================================
 #   WinPE PostOS
 #   Set OOBEDeploy CMD
@@ -84,6 +184,7 @@ $SetCommand = @'
 PowerShell -NoL -Com Set-ExecutionPolicy RemoteSigned -Force
 set path=%path%;C:\Program Files\WindowsPowerShell\Scripts
 start PowerShell -NoL -W Mi
+start "OOBEFixScript" PowerShell -NoL -ExecutionPolicy Bypass -File C:\ProgramData\OSDeploy\OOBEFixScript.ps1
 start "Start-OOBEDeploy" PowerShell -NoL -C Start-OOBEDeploy
 exit
 '@
